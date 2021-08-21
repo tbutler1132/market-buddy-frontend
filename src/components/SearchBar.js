996c35325efc8ade4b0056a3fcc549af63ddd74e
Timothy Butler <tbutler1132@gmail.com>
2021-08-21 07:05:36 -0400

    search bar mvp

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,13 @@
 import React, {useState} from 'react';
 import axios from 'axios'
-import {Autocomplete} from '@material-ui/lab'
-import { TextField } from '@material-ui/core';
 import {useHistory} from 'react-router-dom'
 import {BASE_API} from './../App'
 
 
-function SearchBar(props) {
+function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('')
     const [suggestions, setSuggestions] = useState([])
     const history = useHistory()
-
-    // // const dummy = [
-    // //     {
-    // //         title: "Hey"
-    // //     },
-    // //     {
-    // //         title: "Never"
-    // //     },
-    // // ]
 
 
     const fetchResults = (event, values) => {
@@ -26,14 +15,12 @@ function SearchBar(props) {
         if(searchTerm){
             axios(`${BASE_API}/stocks/search/${searchTerm}`)
             .then(results => {
-                console.log(results.data)
                 setSuggestions(results.data)
             })
         }
     }
 
     const submitHandler = (event, value) => {
-        console.log("Clicked")
         history.push(`/stocks/${event.target.innerText.toLowerCase()}`)
         setSuggestions([])
     }
@@ -48,25 +35,6 @@ function SearchBar(props) {
         }
     }
 
-
-    // console.log(suggestions)
-
-    // return (
-    //     <div>
-    //         <Autocomplete
-    //             id="combo-box-demo"
-    //             handleHomeEndKeys={false}
-    //             onSubmit={(event) => submitHandler(event)}  
-    //             options={suggestions}
-    //             onChange={(event, value) => history.push(`/stocks/${value?.symbol.toLowerCase()}`)}
-    //             getOptionSelected={(option, value) => option.symbol === value.symbol}
-    //             getOptionLabel={(suggestion) => suggestion.symbol}
-    //             onInputChange={fetchResults}
-    //             style={{ width: 300 }}
-    //             renderInput={(params) => <TextField  {...params} label="Search by stock symbol" variant="outlined" />}
-    //         />
-    //     </div>
-    // );
 
     return (
         <div className="wrapper">
