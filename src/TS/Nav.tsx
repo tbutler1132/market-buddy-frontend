705831c2f44b5bbe1a919ac5b7642b7ee03b9881
Timothy Butler <tbutler1132@gmail.com>
2022-04-27 13:06:09 -0400

    local storage

--- src/TS/Nav.tsx
+++ src/TS/Nav.tsx
@@ -3,6 +3,9 @@ import Button from "@mui/material/Button";
 import { toggleDarkMode } from '../app/stylesSlice'
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import Popover from '@mui/material/Popover';
+import { useState } from "react";
+import Typography from '@mui/material/Typography';
 
 import SearchBar from '../components/SearchBar';
 
@@ -16,11 +19,22 @@ function Nav() {
     const history = useHistory()
     const dispatch = useDispatch()
     const { mode } = useSelector((state: any) => state.styles)
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+    const handlePopoverOpen = (e: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(e.currentTarget);
+    };
+  
+    const handlePopoverClose = () => {
+      setAnchorEl(null);
+    };
+  
+    const open = Boolean(anchorEl);
 
     const darkModeHandler = (e: any) => {
-        dispatch(toggleDarkMode(mode === "dark" ? "light" : "dark"))
-        let favoriteMode = mode === "dark" ? "light" : "dark"
-        localStorage.setItem("mode", favoriteMode)
+        let toggleMode = mode === "dark" ? "light" : "dark"
+        dispatch(toggleDarkMode(toggleMode))
+        localStorage.setItem("mode", toggleMode)
     }
 
 
@@ -37,9 +51,35 @@ function Nav() {
                 <div className="search">
                     <SearchBar />
                 </div>
-                <div className="logout-button">
-                    <Button onClick={darkModeHandler}>Toggle dark mode</Button>
-                </div> 
+                    <div className="logout-button">
+                        <Button
+                        aria-owns={open ? 'mouse-over-popover' : undefined}
+                        aria-haspopup="true"
+                        onMouseEnter={handlePopoverOpen}
+                        onMouseLeave={handlePopoverClose} 
+                        onClick={darkModeHandler}>Toggle dark mode</Button>
+                        <Popover
+                        id="mouse-over-popover"
+                        classes={{ paper: "MuiPopover-paper" }}
+                        sx={{
+                        pointerEvents: 'none',
+                        }}
+                        open={open}
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                        }}
+                        onClose={handlePopoverClose}
+                        disableRestoreFocus
+                        >
+                            <Typography sx={{ p: 1 }}>I remember your preferred mode, try refreshing!</Typography>
+                        </Popover>
+                    </div> 
                 {/* <div className="logout-button">
                     <Button onClick={signupHandler}>Login</Button>
                 </div> */}
