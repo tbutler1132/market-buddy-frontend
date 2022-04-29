def6ab8a6d436921c5949600d4b2bccffdad2d94
Timothy Butler <tbutler1132@gmail.com>
2022-04-29 15:06:23 -0400

    removed api dir

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const BASE_API = 'http://localhost:7000'
-
-// export const fetchUser = axios(`${BASE_API}/users/60dfbc675aa6dc13e52fcf4b`)
-
-export const postStock = (stockObj) => axios.post(`${BASE_API}/users/60dfbc675aa6dc13e52fcf4b/stocks`, {stockObj})
-
-export const updateStockHoldings = (userId, stockId, updatedStock) => axios.patch(`${BASE_API}/${userId}/stocks/${stockId}`, updatedStock)
