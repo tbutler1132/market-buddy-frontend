9cc9530301f15703700816251eca348911231c53
Timothy Butler <tbutler1132@gmail.com>
2022-04-30 15:30:57 -0400

    changed chart ranges

--- src/TS/Home.tsx
+++ src/TS/Home.tsx
@@ -7,11 +7,7 @@ import { useState } from 'react';
 import StockGraph from './StockGraph';
 import { useGetCurrentPortfolioValueQuery, useGetHistoricalPortfolioValueQuery, useGetUserQuery, useGetPortfolioDataQuery } from '../app/services/MarketBuddy';
 import ChartNav from './ChartNav';
-
-// interface chartData {
-//     date: string,
-//     value: number
-// }
+import { useEffect } from 'react';
 
 const formatChartData = (data: any, currentData: any) => {
 
@@ -29,10 +25,10 @@ const formatChartData = (data: any, currentData: any) => {
     return arr
 }
 
-const chartRanges = ["ytd", "5d", "1m", "3m", "1y", "5y"]
+const chartRanges = ["ytd", "1m", "3m", "6m"]
 
 function Home() {
-    const [timeRange, setTimeRange] = useState("ytd")
+    const [timeRange, setTimeRange] = useState("1m")
     const { auth } = useSelector((state: any) => state)
     const { data, isLoading } = useGetCurrentPortfolioValueQuery(auth.user)   
     const { data: historicalPortfolioValue, isLoading: historicalPortfolioValueIsLoading } = useGetHistoricalPortfolioValueQuery(auth.user)
@@ -49,7 +45,7 @@ function Home() {
             <div className="row">
                 <div className="col-12">
                     <h1>${data.value.toLocaleString()}</h1>
-                    <StockGraph width={500} height={400} type="value" data={formatChartData(historicalPortfolioValue.historicalPortfolioValue, data)} />
+                    <StockGraph width={500} height={400} type="value" data={formatChartData(historicalPortfolioValue, data)} />
                     <ChartNav chartRanges={chartRanges} setTimeRange={timeRangeClickHandler} timeRange={timeRange}/>
                     <div className="css-1">
                         <h3>Buying power: ${Number(currentUser.cash.toFixed(2)).toLocaleString()}</h3>
